b59bde203a04835f164bd86ab25eeb04cfb6d55c
Siru A <s1a@ucsd.edu>
2017-02-16 11:46:13 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,14 +25,39 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	
 	console.log("User clicked on project " + idNumber);
+	$.get('/project/' + idNumber,addProject);
+	
 }
 
+function addProject(result){
+	console.log(result['id']);
+	var projectHTML =
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +'</small></p>'+
+    '<p>' + result['summary'] + '</p>';
+	$("#project"+result['id']+" .details").html(projectHTML);
+
+
+}
+function callback(result){
+	console.log(result);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette',addColor);
+}
+function addColor(result){
+	var colors = result['colors']['hex'];
+$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
 }
